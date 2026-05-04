7422f5dfe2f800db9af581e92f85f16f1474e8a6
Narendra Wardianto <150513528+Naren56@users.noreply.github.com>
2026-05-04 13:20:14 +0700

    Create Supabase configuration file

---
+++ lib/config/supabase_config.dart
@@ -0,0 +1,4 @@
+const String supabaseUrl = 'https://shoxoghitibiskqxphrj.supabase.co';
+const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNob3hvZ2hpdGliaXNrcXhwaHJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA2NzA4MjYsImV4cCI6MjA4NjI0NjgyNn0.C3tzCRnxcf-wInCo9sVx-xPTCiaVbXsz5gA_aPoWf2c';
+
+// This configuration file contains the Supabase URL and Anon Key used to connect to the Supabase backend.
